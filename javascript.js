2376bbd858dbde7fc5be8b7db43ba07cbf8df90d
NaveenkumarY26 <118093734+NaveenkumarY26@users.noreply.github.com>
2023-11-30 13:10:07 +0530

    commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const hamburger=document.querySelector(".hamburger-icon")
+hamburger.addEventListener("click",togglemenu)
+
+function togglemenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
